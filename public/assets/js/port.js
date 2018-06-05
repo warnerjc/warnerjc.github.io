427bee520dd7f205e206f115bf22ddd55afb8440
Jason Warner <jcwarner@gmail.com>
2018-06-04 22:53:04 -0500

    changed some element positions for better responsiveness

---
+++ public/assets/js/port.js
@@ -0,0 +1,18 @@
+$(document).ready( function() {
+
+// SMOOTH SCROLLING
+$('a[href*="#"]').click(function (event) {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top - 62
+            }, 1000);
+        }
+    }
+});
+
+});
+
